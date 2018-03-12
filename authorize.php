22f0fca676ac6b22fa6b8d8a9e65d1834cff0bd2
nedilio <izquierdonelson@gmail.com>
2018-03-11 22:23:57 -0300

    subiendo tema

---
+++ authorize.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Template Name: Authorize Strava
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+get_header();?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+            <form action="" method="post">
+                <input type="text" name="nombre" class="nombre">
+                <input type="number " name="stravaid" class="stravaid">
+                <input type="text" name="username" class="username">
+                <input type="submit" name="SubmitButton">
+            </form>
+
+			<?php
+if(isset($_POST['SubmitButton'])){ //check if form was submitted
+    $username = $_POST['username']; //get input text
+    $nombre = $_POST['nombre']; //get input text
+    $stravaid = $_POST['stravaid']; //get input text
+    
+    $userdata = array(
+        'user_login'  =>  $username,
+        'display_name'    =>  $nombre,
+        'user_pass'   =>  NULL  // When creating an user, `user_pass` is expected.
+    );
+    $user_id = wp_insert_user( $userdata ) ;
+    
+    //On success
+    if ( ! is_wp_error( $user_id ) ) {
+        // echo "User created : ". $user_id;
+        add_user_meta( $user_id, 'strava_id', $stravaid);
+        echo '<div id="message" class="success"><p>Usuario Creado</p></div>';
+    } else {
+        $error_string = $user_id->get_error_message();
+       echo '<div id="message" class="error"><p>' . $error_string . '</p></div>';
+    }
+  }    
+			?>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<script>
+
+
+$(document).ready(function(){
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+
+    var code = getUrlParameter('code');
+    console.log(code);
+
+    $.post( "https://www.strava.com/oauth/token?client_id=24077&client_secret=5b8452e1e057081f0f189f6ce0aa806c0b0ddadb&code="+code+"", function( data ) {
+        console.log(data);
+        var nombre = data.athlete.firstname+' '+data.athlete.lastname;
+        $('.nombre').val(nombre);
+        var stravaid = data.athlete.id;
+        $('.stravaid').val(stravaid);
+        var username = data.athlete.username;
+        $('.username').val(username);
+        // var pic_url = data.athlete.profile;
+        // $('.profile').attr('src',pic_url);
+        // $('.username').val(data.athlete.username);
+
+        // var id = data.athlete.id;
+        // console.log(id);
+        // $.get( "https://www.strava.com/api/v3/athletes/"+id+"/stats?access_token=1ee5269fa44de9a7709e0ee1798efc1aab4a9ae6", function( data ) {
+        //     console.log(data);
+        //     var km_totales = data.all_ride_totals.distance;
+        //     console.log(km_totales);
+        //     $('.km').text(km_totales/1000);
+        // });
+
+    });
+
+
+});
+</script>
+<?php get_footer();
---
+++ strava.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Template Name: Strava
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+get_header();?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main container" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/page/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				
+
+            endwhile; // End of the loop.
+//             $user = wp_get_current_user();
+// // print_r ($user); 
+// $user_id = $user->ID;
+// // $strava_id = 1326693;
+// // add_user_meta( $user_id, 'strava_id', $strava_id);
+// echo $user->strava_id;
+//  $server = "http://" . $_SERVER['SERVER_NAME']; 
+require_once( $_SERVER['CONTEXT_DOCUMENT_ROOT'] . '/wordpress/' . 'wp-load.php' );
+$membersArray = get_users( 'role=subscriber' );
+?>
+<div class="row">
+<div class="col">
+<?php // Array of WP_User objects.
+    foreach ( $membersArray as $ciclista ){
+      echo '<p>'.( $ciclista->nickname ).': '.$ciclista->strava_id.'</p>';
+    // print_r($ciclista->strava_id);
+    } 
+
+			?>
+</div>
+</div>
+    
+<a href="https://www.strava.com/oauth/authorize?client_id=24077&redirect_uri=http://localhost/wordpress/autorizar/&response_type=code&approval_prompt=auto&scope=public&state">Link</a>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<script>
+$.post( "https://www.strava.com/oauth/token?client_id=24077&client_secret=5b8452e1e057081f0f189f6ce0aa806c0b0ddadb&code=73303d27437b22472cf6fffb6abb25a5fc817a16", function( data ) {
+    $( "#strava" ).html( data );  
+    console.log(data.athlete.username);
+    var pic_url = data.athlete.profile;
+    $('.profile').attr('src',pic_url);
+    $('.username').text(data.athlete.username);
+
+    var id = data.athlete.id;
+    console.log(id);
+    $.get( "https://www.strava.com/api/v3/athletes/"+id+"/stats?access_token=1ee5269fa44de9a7709e0ee1798efc1aab4a9ae6", function( data ) {
+        console.log(data);
+        var km_totales = data.all_ride_totals.distance;
+        console.log(km_totales);
+        $('.km').text(km_totales/1000);
+    });
+
+});
+
+
+
+
+</script>
+<?php get_footer();
